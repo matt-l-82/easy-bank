eb75238b9996c75678c14a43742c68c96182e31c
Matt <mattlaws1982@gmail.com>
2020-08-19 08:56:22 +0100

    animated burger menu complete, fade-in / out of overlay complete

---
+++ app/js/script.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+
+    if(header.classList.contains('open')){ // Close hamburger menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } else {
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+    
+});
